9926ae33d76132658b5df6fca1a9ddf0c2611df9
sam8e1ito <samvelsymonian@gmail.com>
2026-07-17 01:18:03 +0400

    ui&refactor: add styles and a new optional prop startOption

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,8 +1,10 @@
 import type { SelectHTMLAttributes } from 'react'
 import type { Option } from '@/types/types'
+import clsx from 'clsx'
 
 type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
     label?: string
+    startOption?: string
     options: Option[]
     error?: string
 }
@@ -10,16 +12,24 @@ type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
 export default function Select({
     label,
     options,
+    startOption,
     error,
+    className,
     ...props
 }: SelectProps) {
     return (
         <label>
             {label && <div>{label}</div>}
 
-            <select {...props}>
+            <select
+                {...props}
+                className={clsx(
+                    'w-full rounded-lg border-2 border-border-accent px-1 py-2 placeholder:text-text-light-secondary',
+                    className
+                )}
+            >
                 <option value="" disabled>
-                    Select {label?.toLowerCase()}
+                    Select {label ? label.toLowerCase() : startOption}
                 </option>
                 {options.map((opt) => (
                     <option key={opt.value} value={opt.value}>
