2be9681d8f44f684a82bc8909fb46098d1089518
sam8e1ito <samvelsymonian@gmail.com>
2026-07-17 01:04:42 +0400

    ui: add styling to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,24 @@
+import clsx from 'clsx'
+
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     children: React.ReactNode
 }
 
-export default function Button({ children, ...props }: ButtonProps) {
-    return <button {...props}>{children}</button>
+export default function Button({
+    children,
+    className,
+    disabled,
+    ...props
+}: ButtonProps) {
+    return (
+        <button
+            {...props}
+            className={clsx(
+                `text-text-dark py-2 w-full rounded-xl ${disabled ? 'bg-gray-300' : 'bg-accent'} hover:cursor-pointer`,
+                className
+            )}
+        >
+            {children}
+        </button>
+    )
 }
