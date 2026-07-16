e173827318ec34cab00d66146f9dcd12207bb653
sam8e1ito <samvelsymonian@gmail.com>
2026-07-17 01:15:34 +0400

    UI: add styles to Question component

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -5,9 +5,9 @@ type QuestionProps = {
 
 export default function Question({ question, children }: QuestionProps) {
     return (
-        <div>
-            <h3>{question}</h3>
-            <div>{children}</div>
+        <div className="bg-bg-secondary rounded-xl border-2 border-border-accent p-4 text-center">
+            <h3 className="text-2xl text-center mb-4">{question}</h3>
+            <div className="flex flex-col gap-4">{children}</div>
         </div>
     )
 }
