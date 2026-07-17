635ad8c3ec3a466c2353a0f33231eb5cc306101c
sam8e1ito <samvelsymonian@gmail.com>
2026-07-17 19:07:58 +0400

    fix: sort sets from first to last

--- src/services/training.ts
+++ src/services/training.ts
@@ -1,19 +1,30 @@
-import { supabase } from '@/lib/supabase'
+import { supabase } from "@/lib/supabase"
 
 export async function getTrainingPlan(userId: string) {
     const { data, error } = await supabase
         .from('workout_plans')
-        .select(
-            `
+        .select(`
             *,
             workout_exercises (
                 *,
                 workout_sets (*)
             )
-        `
-        )
+        `)
         .eq('user_id', userId)
 
     if (error) throw error
+
+    data?.forEach((workout) => {
+        workout.workout_exercises.sort(
+            (a: any, b: any) => a.order_index - b.order_index
+        )
+
+        workout.workout_exercises.forEach((exercise: any) => {
+            exercise.workout_sets.sort(
+                (a: any, b: any) => a.set_number - b.set_number
+            )
+        })
+    })
+
     return data
 }
