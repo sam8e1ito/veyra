1ef5c040696004e7177f703ba3ffccdc114913d9
sam8e1ito <samvelsymonian@gmail.com>
2026-07-22 01:23:16 +0400

    feat: create function to show ui-friendly date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 export function getDateKey(date: Date = new Date()): string {
     return date.toLocaleDateString('en-CA')
 }
@@ -6,4 +8,8 @@ export function getCurrentDayOfWeek() {
     const day = new Date().getDay()
 
     return day === 0 ? 7 : day
+}
+
+export function formatDate(date: string) {
+    return format(new Date(date), 'EEEE, MMM d')
 }
